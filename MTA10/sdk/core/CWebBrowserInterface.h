a89dd9f1e6aa1d483ecf321137e1029e479bf796
Jusonex <jusonex96@gmail.com>
2014-04-17 17:05:50 +0200

    Added missing file

---
+++ MTA10/sdk/core/CWebBrowserInterface.h
@@ -0,0 +1,48 @@
+/*****************************************************************************
+*
+*  PROJECT:     Multi Theft Auto v1.0
+*               (Shared logic for modifications)
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        sdk/core/CWebBrowserInterface.h
+*  PURPOSE:     Awesomium interface class
+*
+*****************************************************************************/
+
+#ifndef __CWEBBROWSERINTERFACE_H
+#define __CWEBBROWSERINTERFACE_H
+
+namespace Awesomium
+{
+    class WebView;
+    class WebString;
+}
+class CWebBrowserItem;
+
+class CWebBrowserInterface
+{
+public:
+    virtual Awesomium::WebView* CreateWebView(unsigned int uiWidth, unsigned int uiHeight) = 0;
+    virtual void Update() = 0;
+    
+    virtual bool IsLoading(CWebBrowserItem* pWebBrowserItem) = 0;
+    virtual void GetPageTitle(CWebBrowserItem* pWebBrowserItem, SString& outPageTitle) = 0;
+    virtual void GetPageURL(CWebBrowserItem* pWebBrowserItem, SString& outURL) = 0;
+
+    virtual void GetScrollPosition(CWebBrowserItem* pWebBrowserItem, int& iScrollX, int& iScrollY) = 0;
+    virtual void SetScrollPosition(CWebBrowserItem* pWebBrowserItem, int iScrollX, int iScrollY) = 0;
+
+    virtual void InjectMouseMove(CWebBrowserItem* pWebBrowserItem, int iPosX, int iPosY) = 0;
+    virtual void InjectMouseDown(CWebBrowserItem* pWebBrowserItem, int mouseButton) = 0;
+    virtual void InjectMouseUp(CWebBrowserItem* pWebBrowserItem, int mouseButton) = 0;
+    virtual void InjectKeyboardEvent(CWebBrowserItem* pWebBrowserItem, const SString& strKey, bool bKeyDown = true, bool bCharacter = false) = 0;
+
+    virtual bool LoadURL(CWebBrowserItem* pWebBrowserItem, const SString& strURL) = 0;
+    virtual bool IsURLAllowed(const Awesomium::WebString& strURL) = 0;
+    virtual void ClearWhitelist() = 0;
+    virtual void AddAllowedPage(const SString& strURL) = 0;
+    virtual void RequestPages(const std::vector<SString>& pages) = 0;
+    virtual void AllowPendingPages() = 0;
+    virtual void DenyPendingPages() = 0;
+};
+
+#endif
